fb1e54333192210bbf9aa8601237859d795a9bd3
hamiltonquintero <146398485+hamiltonquintero@users.noreply.github.com>
2025-10-01 19:21:01 -0500

    Update CamaleonBasico.java

--- CamaleonBasico.java
+++ CamaleonBasico.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+
+public class CamaleonBasico {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.print("Cantidad de números a validar: ");
+        int cantidad = sc.nextInt();
+
+        for (int i = 0; i < cantidad; i++) {
+            int numero = rand.nextInt(90000) + 10000; 
+            int suma = 0;
+            int invertido = 0;
+            int copia = numero;
+
+            while (copia > 0) {
+                int digito = copia % 10;
+                suma += digito;
+                invertido = invertido * 10 + digito;
+                copia /= 10;
+            }
+
+            boolean esCamaleon = (suma % 2 == 0) && (invertido % 3 == 0);
+            System.out.println(numero + " > " + (esCamaleon ? "Sí" : "No"));
+        }
+
+        sc.close();
+    }
+}
 
