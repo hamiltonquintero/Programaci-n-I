2c241761fab66f5d73ef444d667f285459ec1c0e
hamiltonquintero <146398485+hamiltonquintero@users.noreply.github.com>
2025-10-29 15:25:58 -0500

    se organiza el codigo

--- programacion/TarjetaProcesador.java
+++ programacion/TarjetaProcesador.java
@@ -1,13 +1,14 @@
 package com.mycompany.programacion;
 
 public class TarjetaProcesador implements IProcesadorPago {
-    // Podrías agregar aquí atributos como numeroTarjeta, cvv, etc.
+    
     
     @Override
     public boolean procesar(double monto) {
-        // Lógica de validación compleja (simulada)
+       
         System.out.println("LOG: Contactando pasarela de pago y verificando tarjeta...");
-        // Simulamos un 90% de éxito en pagos con tarjeta
+        
         return Math.random() < 0.9;
     }
+
 }
