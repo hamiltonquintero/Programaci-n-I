162490f367d4798cd618c9950aed2d759346f53c
hamiltonquintero <146398485+hamiltonquintero@users.noreply.github.com>
2025-10-29 15:17:38 -0500

    se da explicacion del codigo
    Updated comment for clarity in procesar method.

--- programacion/EfectivoProcesador.java
+++ programacion/EfectivoProcesador.java
@@ -4,7 +4,7 @@ public class EfectivoProcesador implements IProcesadorPago {
     @Override
     public boolean procesar(double monto) {
         System.out.println("LOG: Verificando monto en efectivo...");
-        // En efectivo, el pago siempre es exitoso si el monto es > 0.
+        // se dice que si en efectivo, el pafo siempre es exitoso siempre y cuando el monto sea mayo que cero
         return monto > 0;
     }
 }
