832c67d29ffd28402b854a8a80f1de21669fea13
hamiltonquintero <146398485+hamiltonquintero@users.noreply.github.com>
2025-10-29 15:22:26 -0500

    se organiza el codigo

--- programacion/Pago.java
+++ programacion/Pago.java
@@ -4,13 +4,11 @@ import java.util.Date;
 
 public class Pago {
     private Pedido pedido; 
-    // CAMBIO 1: El atributo ahora es de tipo Interfaz
     private IProcesadorPago procesador; 
     private String metodoPago; 
     private Date fechaPago;
     private double monto;
 
-    // CAMBIO 2: El constructor recibe un objeto que implementa la interfaz
     public Pago(Pedido pedido, IProcesadorPago procesador, String metodoPago) {
         this.pedido = pedido;
         this.metodoPago = metodoPago;
@@ -18,11 +16,9 @@ public class Pago {
         this.monto = pedido.getTotal() + pedido.getInformacionEnvio().getCostoEnvio(); 
     }
 
-    // CAMBIO 3: Delega el trabajo. Usa el método procesar() de la Interfaz.
     public boolean procesarPago() {
         System.out.printf("Iniciando pago de $%.2f con %s...\n", monto, metodoPago);
         
-        // Llama al método procesar() del objeto específico (TarjetaProcesador o EfectivoProcesador)
         if (procesador.procesar(monto)) {
             this.fechaPago = new Date();
             pedido.actualizarEstado("Pagado"); 
@@ -35,7 +31,6 @@ public class Pago {
         }
     }
     
-    // Getters
     public Pedido getPedido() { return pedido; }
     public String getMetodoPago() { return metodoPago; }
     public Date getFechaPago() { return fechaPago; }
@@ -44,4 +39,5 @@ public class Pago {
     public void emitirRecibo() {
         System.out.println("Recibo emitido para el pedido " + pedido.getCliente().getNombre());
     }
+
 }
