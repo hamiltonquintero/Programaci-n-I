c8b0c27eaf6cd990a5a9873eefd115f8e45d5566
hamiltonquintero <146398485+hamiltonquintero@users.noreply.github.com>
2025-11-08 20:08:26 -0500

    Add Administrador class extending Usuario

--- programacion/Administrador.java
+++ programacion/Administrador.java
@@ -1,1 +1,20 @@
+package com.mycompany.programacion;
+
+public class Administrador extends Usuario { 
+    private String areaResponsable;
+
+    public Administrador(String nombre, String email, String contrasena, String areaResponsable) {
+       
+        super(nombre, email, contrasena);
+        this.areaResponsable = areaResponsable;
+    }
+
+    @Override
+    public void actualizarDatos() {
+       
+        System.out.println("ADMIN: Actualizando credenciales del administrador: " + nombre); 
+    }
+    
+  
+}
 
