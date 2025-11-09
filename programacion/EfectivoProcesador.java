5783f1917b0b3fc9ff449b30764cf0a5286d93c3
ariashenaomanuela-cmy <ariashenaomanuela@gmail.com>
2025-11-08 21:04:51 -0500

    se actualizan comentarios
    Added a comment to clarify payment processing logic.

--- programacion/EfectivoProcesador.java
+++ programacion/EfectivoProcesador.java
@@ -4,7 +4,9 @@ public class EfectivoProcesador implements IProcesadorPago {
     @Override
     public boolean procesar(double monto) {
         System.out.println("LOG: Verificando monto en efectivo...");
-        // se dice que si en efectivo, el pafo siempre es exitoso siempre y cuando el monto sea mayo que cero
+        // se dice que si en efectivo, el pago siempre es exitoso siempre y cuando el monto sea mayo que cero
+        //si es mayor que 0 se considera que el cliente no tiene suficiente efectivo
         return monto > 0;
     }
 }
+
