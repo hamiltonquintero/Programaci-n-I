84ea940e59551d5c7d9dabc46a125a3b7170d0e7
ariashenaomanuela-cmy <ariashenaomanuela@gmail.com>
2025-11-08 20:57:15 -0500

    se agregan comentarios
    Added comments to describe the purpose and functionality of the Descuento class and its methods.

--- programacion/Descuento.java
+++ programacion/Descuento.java
@@ -2,12 +2,16 @@
 package com.mycompany.programacion;
 
 import java.util.Date;
+//revisando por (nombre) el (fecha) 
+//esta clase representa un descuento aplicable a productos dentro de un rango de fechas 
 
+//se inicia los datos de descuento incluyendo su vigencia y porcentaje 
 public class Descuento {
     private String descripcion;
     private double porcentaje;
     private Date fechaInicio;
     private Date fechaFin;
+    
 
     public Descuento(String descripcion, double porcentaje, Date fechaInicio, Date fechaFin) {
         this.descripcion = descripcion;
@@ -21,7 +25,9 @@ public class Descuento {
         
         return ahora.after(fechaInicio) && ahora.before(fechaFin);
     }
+    
 
+    //se aplica el descuento solo si esta activo. Si no, delvuelve el precio original (no esta activo)
     public double aplicarDescuento(double precioOriginal) {
         if (estaActivo()) {
            
@@ -30,4 +36,5 @@ public class Descuento {
         return precioOriginal;
     }
 }
+
 
